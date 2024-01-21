f76e347e9153bc560f3abb6fa1fada52c3f6bf83
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 21:19:34 +0900

    ToDoアプリ作成

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskData = [(title: "ジョギングする", completed: false),
+                           (title: "お花に水をやる", completed: false),
+                           (title: "勉強する", completed: false),
+                           (title: "仕事する", completed: false)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<taskData.count, id: \.self){index in
+                Button{
+                    taskData[index].completed.toggle()
+                }
+                label :
+                {
+                    HStack{
+                        Image(systemName: taskData[index].completed == true ? "checkmark.circle.fill" : "circle")
+                        Text(taskData[index].title)
+                    }
+                }.foregroundColor(.black)
+                
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
     }
 }
 
